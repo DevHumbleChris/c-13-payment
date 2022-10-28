b60e9d02c24500e1aafd3c175b68b0c9f0a1c0ab
DevHumbleChris <dev.humblechris254@gmail.com>
2022-10-28 16:49:06 +0300

    Added Error 404 Page

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -7,6 +7,7 @@ import Login from "../views/Login";
 import Header from "./Header";
 import Signup from "../views/Signup";
 import ProtectedRoute from "./ProtectedRoute";
+import Error404 from "../views/404";
 
 export default function Navigation() {
   const authenticated = useSelector((state) => state.auth.authenticated);
@@ -20,6 +21,7 @@ export default function Navigation() {
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
